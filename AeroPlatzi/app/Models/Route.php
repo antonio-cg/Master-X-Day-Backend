968d979cda5f32b9e24b7689f8ef6bd989922660
antonio-cg <antoniocg507@gmail.com>
2021-03-06 16:22:01 -0600

    typo in route model

--- AeroPlatzi/app/Models/Route.php
+++ AeroPlatzi/app/Models/Route.php
@@ -10,7 +10,7 @@ class Route extends Model
     protected $primaryKey = 'IDRoute';
 
     protected $fillable = [
-        'IDDepartured', 'IDArrived', 'name',
+        'IDDepartured', 'IDArrieved', 'Name',
     ];
 
     protected $hidden = [];
